0360ee0c1f008f55ceeb3461fd91ef0ffd8e4004
kws04181 <kimsungwon57@naver.com>
2023-02-10 18:14:39 +0900

    #7.6 Parameters

---
+++ my-app/src/routes/Detail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
+function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      <h1>디테일</h1>
+    </div>
+  );
+}
+
+export default Detail;
